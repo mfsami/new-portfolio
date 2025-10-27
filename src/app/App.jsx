30e323c34fc4bc6c7c3eee2b5264675be5b0babe
Sami El-sayed <samielsayed39@gmail.com>
2025-10-27 11:02:21 -0600

    Added js to scroll to top of accessed page

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -16,28 +16,32 @@ import G2ARedesign from "../pages/projects/G2ARedesign";
 import ParryGame from "../pages/projects/ParryGame";
 import SpaceExploration from "../pages/projects/SpaceExploration";
 
+// JS
+import ScrollToTop from "../../js/scrollToTop";
+
 export default function App() {
   return (
-    <Routes>
-      <Route element={<RootLayout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/til" element={<TIL />} />
-        <Route path="/projects/last-stand" element={<LastStand />} />
-        <Route path="/projects/heartbound" element={<Heartbound />} />
-        <Route path="/projects/winter-soccer" element={<WinterSoccer />} />
-        <Route path="/projects/portfolio" element={<Portfolio />} />
-        <Route path="/projects/graphite" element={<Graphite />} />
-        <Route path="/projects/g2a-redesign" element={<G2ARedesign />} />
-        <Route path="/projects/parry-game" element={<ParryGame />} />
-        <Route
-          path="/projects/space-exploration"
-          element={<SpaceExploration />}
-        />
-      </Route>
-
-      {/* 404 fallback */}
-      <Route path="*" element={<h2> nothing here...</h2>} />
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route element={<RootLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/til" element={<TIL />} />
+          <Route path="/projects/last-stand" element={<LastStand />} />
+          <Route path="/projects/heartbound" element={<Heartbound />} />
+          <Route path="/projects/winter-soccer" element={<WinterSoccer />} />
+          <Route path="/projects/portfolio" element={<Portfolio />} />
+          <Route path="/projects/graphite" element={<Graphite />} />
+          <Route path="/projects/g2a-redesign" element={<G2ARedesign />} />
+          <Route path="/projects/parry-game" element={<ParryGame />} />
+          <Route
+            path="/projects/space-exploration"
+            element={<SpaceExploration />}
+          />
+        </Route>
+        <Route path="*" element={<h2> nothing here...</h2>} />
+      </Routes>
+    </>
   );
 }
